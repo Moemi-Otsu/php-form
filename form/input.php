d6a2b490a24c917e4f8e1aaf8ad21479d2531a08
moemi ohtsu <s0623192@gmail.com>
2021-01-17 14:33:01 +0900

    XSS, クリックジャッキングをサニタライズ

--- form/input.php
+++ form/input.php
@@ -1,12 +1,18 @@
 <?php
 
+header('X-Frame-Options: DENY');
+
 // スーパーグローバル変数 php 9種類
 // 連想配列
 
-if(!empty($_POST)){
-  echo '<pre>';
-  var_dump($_POST);
-  echo '</pre>';
+//if(!empty($_POST)){
+//  echo '<pre>';
+//  var_dump($_POST);
+//  echo '</pre>';
+//}
+
+function h($str){
+		return htmlspecialchars($str, ENT_QUOTES, 'UTF-8');
 }
 
 // 入力、確認、完了 input.php, confirm.php, thanks.php
@@ -33,17 +39,17 @@ if(!empty($_POST['btn_submit'])){
 
 <form method="POST" action="input.php">
 		氏名
-		<?php echo $_POST['your_name']; ?>
+		<?php echo h($_POST['your_name']); ?>
 		<br>
 		メールアドレス
-		<?php echo $_POST['email']; ?>
+		<?php echo h($_POST['email']); ?>
 		<br>
 		<!-- 戻るボタン -->
 		<input type="submit" name="back" value="戻る"
 		<!-- 戻るボタン -->
 		<input type="submit" name="btn_submit" value="送信する">
-		<input type="hidden" name="your_name" value="<?php echo $_POST['your_name']; ?>">
-		<input type="hidden" name="email" value="<?php echo $_POST['email']; ?>">
+		<input type="hidden" name="your_name" value="<?php echo h($_POST['your_name']); ?>">
+		<input type="hidden" name="email" value="<?php echo h($_POST['email']); ?>">
 </form>
 
 <?php endif; ?>
@@ -60,10 +66,10 @@ if(!empty($_POST['btn_submit'])){
 
 <form method="POST" action="input.php">
   氏名
-  <input type="text" name="your_name" value="<?php if(!empty($_POST['your_name'])){ echo $_POST['your_name']; } ?>">
+  <input type="text" name="your_name" value="<?php if(!empty($_POST['your_name'])){ echo h($_POST['your_name']); } ?>">
   <br>
 		メールアドレス
-		<input type="email" name="email" value="<?php if(!empty($_POST['email'])){ echo $_POST['email']; } ?>">
+		<input type="email" name="email" value="<?php if(!empty($_POST['email'])){ echo h($_POST['email']); } ?>">
 		<br>
   <input type="submit" name="btn_confirm" value="確認する">
 		
