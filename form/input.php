0bac46b1c372f510708ae9d57c49d40393ce9aaf
moemi ohtsu <s0623192@gmail.com>
2021-01-20 16:18:04 +0900

    wip

--- form/input.php
+++ form/input.php
@@ -96,6 +96,24 @@ $token = $_SESSION['csrfToken'];
 		メールアドレス
 		<input type="email" name="email" value="<?php if(!empty($_POST['email'])){ echo h($_POST['email']); } ?>">
 		<br>
+		ホームページ
+		<input type="url" name="url" value="<?php if(!empty($_POST['url'])){ echo h($_POST['url']); } ?>">
+		<br>
+		性別
+		<input type="radio" name="gender" value="0">男性
+		<input type="radio" name="gender" value="1">女性
+		年齢
+		<select name="age">
+				<option value="">選択してください</option>
+				<option value="1">〜19歳</option>
+				<option value="2">20歳〜29歳</option>
+				<option value="3">30歳〜39歳</option>
+				<option value="4">〜19歳</option>
+				<option value="5">〜19歳</option>
+				<option value="6">〜19歳</option>
+		</select>
+		お問い合わせ内容
+		注意事項のチェック
   <input type="submit" name="btn_confirm" value="確認する">
 		<!-- 下記の記述で、valueのなかにトークンが入ってくる -->
 		<input type="hidden" name="csrf" value="<?php echo $token; ?>">
