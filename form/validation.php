c65d2b2cc329cb76fcb43f2e706413274f2e1119
moemi ohtsu <s0623192@gmail.com>
2021-01-27 13:44:45 +0900

    微調整

--- form/validation.php
+++ form/validation.php
@@ -4,8 +4,8 @@ function validation($request){  //$_POST連想配列
 		
 		$errors = [];
 		
-		if(empty($request['your_name'])){
-				$errors[] = '指名は必須です';
+		if(empty($request['your_name']) || 20 < mb_strlen($request['your_name'])){
+				$errors[] = '「氏名」は必須です。20文字以内で入力してください。';
 		}
 		
 		return $errors;
